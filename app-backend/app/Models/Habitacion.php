3116d3b2f0e154efe3d7d08e9c6da45fd06f6aee
Oscar Diaz <oscarock02@gmail.com>
2025-03-23 17:50:24 -0500

    Agrega nombre tablas en los modelos

--- app-backend/app/Models/Habitacion.php
+++ app-backend/app/Models/Habitacion.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Habitacion extends Model
 {
     use HasFactory;
+
+    protected $table = "habitaciones";
     protected $fillable = ['hotel_id', 'tipo', 'acomodacion', 'cantidad'];
-    
+
     public function hotel() {
         return $this->belongsTo(Hotel::class);
     }
--- app-backend/app/Models/Hotel.php
+++ app-backend/app/Models/Hotel.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Hotel extends Model
 {
     use HasFactory;
+
+    protected $table = "hoteles";
     protected $fillable = ['nombre', 'direccion', 'ciudad', 'nit', 'numero_habitaciones'];
-    
+
     public function habitaciones() {
         return $this->hasMany(Habitacion::class);
     }
